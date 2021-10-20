862454eab873c096f6039eed1ce3e994d1df480f
ansango <anibalsantosgo@gmail.com>
2021-10-20 20:34:14 +0200

    reviews

--- components/ui/blocks/Review.tsx
+++ components/ui/blocks/Review.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react"
+import { FC, useState } from "react"
 import { StarIconEmpty, StarIconFull } from "../icons";
 import Image from 'next/image';
 
@@ -23,7 +23,7 @@ type ReviewProps = {
 const Rating = ({ rating }: { rating: number }) => {
     const maxRating: number = 5;
     return (
-        <div className="flex items-center justify-center">
+        <div className="flex">
             {Array.from({ length: rating }, (_, i) => i + 1).map(i => (
                 <StarIconFull key={i} />
             ))}
@@ -34,39 +34,61 @@ const Rating = ({ rating }: { rating: number }) => {
     );
 };
 
-const Avatar = ({ img }: { img: string }) => {
-    return (
-        <div className="flex items-center">
-            <Image src={img} alt="avatar" className="rounded-full mr-4" width={48} height={48} />
-        </div>
-    );
+const Avatar = ({ img, w, h }: { img: string, w: number, h: number }) => {
+    return <Image src={img} alt="avatar" className="rounded-full mr-4" width={w} height={h} />
+
 };
 
 const Review: FC<ReviewProps> = ({ user, review }) => {
-
+    const [isActive, setIsActive] = useState(false)
     const { rating, content, date } = review;
     const { avatar, fullName } = user;
 
     return (
-        <div className="space-y-5">
-            <div className="grid grid-cols-3">
-                <div className="grid grid-cols-2">
-                    <Rating rating={rating} />
-                    <Avatar img={avatar} />
+        <>
+            <div className="md:hidden p-5 space-y-3">
+                <div className="flex items-center space-x-5">
+                    <Avatar img={avatar} w={48} h={48} />
+                    <div>
+                        <p>{fullName}</p>
+                        <Rating rating={rating} />
+                    </div>
+
                 </div>
-                <div className="space-y-3">
-                    <p>{fullName}</p>
-                    <p className="text-gray-300 truncate">{content}</p>
-                    <button className="px-3 py-2 border border-primary rounded-md">
-                        Ver respuesta completa
-                    </button>
-                </div>
-                <div className="flex items-center justify-center">
-                    <p className="text-gray-300">{date}</p>
-                </div>
+                <p className="text-gray-300">{date}</p>
+                {!isActive && <p className="text-gray-300 line-clamp-3">{content}</p>}
+                {isActive && <p className="text-gray-300">{content}</p>}
+                <button className="px-3 py-2 border border-primary rounded-md" onClick={() => setIsActive(!isActive)}>
+                    {!isActive && <span>Ver respuesta completa</span>}
+                    {isActive && <span>Ocultar respuesta completa</span>}
+                </button>
             </div>
-            <hr className="py-1" />
-        </div>
+            <div className="hidden md:block space-y-5">
+                <div className="grid grid-cols-3">
+                    <div className="grid grid-cols-2">
+                        <div className="flex justify-center items-center">
+                            <Rating rating={rating} />
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <Avatar img={avatar} w={56} h={56} />
+                        </div>
+                    </div>
+                    <div className="space-y-3">
+                        <p>{fullName}</p>
+                        {!isActive && <p className="text-gray-300 line-clamp-3">{content}</p>}
+                        {isActive && <p className="text-gray-300">{content}</p>}
+                        <button className="px-3 py-2 border border-primary rounded-md" onClick={() => setIsActive(!isActive)}>
+                            {!isActive && <span>Ver respuesta completa</span>}
+                            {isActive && <span>Ocultar respuesta completa</span>}
+                        </button>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <p className="text-gray-300">{date}</p>
+                    </div>
+                </div>
+                <hr className="py-1" />
+            </div>
+        </>
     )
 }
 
