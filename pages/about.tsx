46df87edf45cf8eb9964e987a7eda5b2021f20cc
ansango <anibalsantosgo@gmail.com>
2021-10-26 01:19:05 +0200

    gallery

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,7 +3,6 @@ import { useI18n } from "next-localization";
 import PageSeo from "components/utils/PageSeo";
 import DefaultLayout from "layouts/DefaultLayout";
 import { Title } from "components/ui/titles";
-import { ContainerContent } from "components/ui/containers";
 
 const About: NextPage = () => {
   const { t } = useI18n();
@@ -11,9 +10,232 @@ const About: NextPage = () => {
     <DefaultLayout>
       <PageSeo title={t("about.seo.title")} description={t("about.seo.description")} />
       <main>
-        <ContainerContent>
-          <Title text={t("about.title")} />
-        </ContainerContent>
+        <section className=" ">
+          <div className="container max-w-xl p-6 py-12 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
+            <div>
+              <h1 className="text-3xl font-bold tracking-tight text-center sm:text-5xl ">
+                <Title text={t("about.title")} />
+              </h1>
+              <p className="max-w-3xl mx-auto mt-4 text-xl text-center ">
+                Quando cetero his ne, eum admodum sapientem ut.
+              </p>
+            </div>
+            <div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+              <div>
+                <h2 className="text-2xl font-bold tracking-tight sm:text-3xl ">
+                  Ad vix debet docendi
+                </h2>
+                <p className="mt-3 text-lg ">
+                  Ne dicta praesent ocurreret has, diam theophrastus at pro. Eos etiam regione ut,
+                  persius eripuit quo id. Sit te euismod tacimates.
+                </p>
+                <div className="mt-12 space-y-12">
+                  <div className="flex">
+                    <div className="flex-shrink-0">
+                      <div className="flex items-center justify-center w-12 h-12 rounded-md bg-violet-600 ">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          className="w-7 h-7"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M5 13l4 4L19 7"
+                          ></path>
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="ml-4">
+                      <h3 className="text-lg font-medium leading-6 ">Per ei quaeque sensibus</h3>
+                      <p className="mt-2 ">
+                        Ex usu illum iudico molestie. Pro ne agam facete mediocritatem, ridens
+                        labore facete mea ei. Pro id apeirian dignissim.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex">
+                    <div className="flex-shrink-0">
+                      <div className="flex items-center justify-center w-12 h-12 rounded-md bg-violet-600 ">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          className="w-7 h-7"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M5 13l4 4L19 7"
+                          ></path>
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="ml-4">
+                      <h3 className="text-lg font-medium leading-6 ">
+                        Cu imperdiet posidonium sed
+                      </h3>
+                      <p className="mt-2 ">
+                        Amet utinam aliquando ut mea, malis admodum ocurreret nec et, elit tibique
+                        cu nec. Nec ex maluisset inciderint, ex quis.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex">
+                    <div className="flex-shrink-0">
+                      <div className="flex items-center justify-center w-12 h-12 rounded-md bg-violet-600 ">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          className="w-7 h-7"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M5 13l4 4L19 7"
+                          ></path>
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="ml-4">
+                      <h3 className="text-lg font-medium leading-6 ">
+                        Nulla omittam sadipscing mel ne
+                      </h3>
+                      <p className="mt-2 ">
+                        At sed possim oporteat probatus, justo graece ne nec, minim commodo legimus
+                        ut vix. Ut eos iudico quando soleat, nam modus.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div aria-hidden="true" className="mt-10 lg:mt-0">
+                <img
+                  src="https://source.unsplash.com/random/360x480"
+                  alt=""
+                  className="mx-auto rounded-lg shadow-lg "
+                />
+              </div>
+            </div>
+            <div>
+              <div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+                <div className="lg:col-start-2">
+                  <h2 className="text-2xl font-bold tracking-tight sm:text-3xl ">
+                    Eam nibh gloriatur ex
+                  </h2>
+                  <p className="mt-3 text-lg ">
+                    Per odio fabellas consulatu cu. Utroque detracto mel ea, quo te latine
+                    theophrastus. Ea his tale nibh dissentias, mei exerci tamquam euripidis cu.
+                  </p>
+                  <div className="mt-12 space-y-12">
+                    <div className="flex">
+                      <div className="flex-shrink-0">
+                        <div className="flex items-center justify-center w-12 h-12 rounded-md bg-violet-600 ">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            className="w-7 h-7"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M5 13l4 4L19 7"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="ml-4">
+                        <h3 className="text-lg font-medium leading-6 ">
+                          Cibo augue offendit has ad
+                        </h3>
+                        <p className="mt-2 ">
+                          An per velit appellantur, ut utinam minimum nominavi sit, odio nostro
+                          habemus ne nec. Ne sonet regione contentiones est.
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex">
+                      <div className="flex-shrink-0">
+                        <div className="flex items-center justify-center w-12 h-12 rounded-md bg-violet-600 ">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            className="w-7 h-7"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M5 13l4 4L19 7"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="ml-4">
+                        <h3 className="text-lg font-medium leading-6 ">
+                          At eum ferri luptatum lobortis
+                        </h3>
+                        <p className="mt-2 ">
+                          Te per quidam maiorum ocurreret, etiam delicatissimi usu ad. Ne has quod
+                          periculis. Te sit primis iisque efficiantur.
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex">
+                      <div className="flex-shrink-0">
+                        <div className="flex items-center justify-center w-12 h-12 rounded-md bg-violet-600 ">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            className="w-7 h-7"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M5 13l4 4L19 7"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="ml-4">
+                        <h3 className="text-lg font-medium leading-6 ">
+                          Dicunt verterem evertitur eu sea
+                        </h3>
+                        <p className="mt-2 ">
+                          Audire principes rationibus eam an, autem nominavi luptatum per te. Sumo
+                          fabulas vim eu, sonet saperet eleifend ut vix.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-10 lg:mt-0 lg:col-start-1 lg:row-start-1">
+                  <img
+                    src="https://source.unsplash.com/random/361x481"
+                    alt=""
+                    className="mx-auto rounded-lg shadow-lg "
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </DefaultLayout>
   );
--- pages/products.tsx
+++ pages/products.tsx
@@ -1,9 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 import type { GetStaticProps, NextPage } from "next";
 import { useI18n } from "next-localization";
 import PageSeo from "components/utils/PageSeo";
 import DefaultLayout from "layouts/DefaultLayout";
 import { Title } from "components/ui/titles";
-import { ContainerContent } from "components/ui/containers";
 
 const Products: NextPage = () => {
   const { t } = useI18n();
@@ -11,9 +11,72 @@ const Products: NextPage = () => {
     <DefaultLayout>
       <PageSeo title={t("products.seo.title")} description={t("products.seo.description")} />
       <main>
-        <ContainerContent>
-          <Title text={t("products.title")} />
-        </ContainerContent>
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 py-24 mx-auto flex flex-wrap">
+            <div className="flex flex-col w-full mb-20 items-start space-y-10">
+              <Title text={t("products.title")} />
+              <p className="lg:w-2/3 leading-relaxed text-base">
+                Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway
+                tile poke farm-to-table. Franzen you probably havent heard of them man bun deep
+                jianbing selfies heirloom.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="pb-6  ">
+          <div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4">
+            <img
+              src="https://source.unsplash.com/301x301/?random"
+              alt=""
+              className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 "
+            />
+            <img
+              alt=""
+              className="w-full h-full rounded shadow-sm min-h-48 "
+              src="https://source.unsplash.com/200x200/?random"
+            />
+            <img
+              alt=""
+              className="w-full h-full rounded shadow-sm min-h-48 "
+              src="https://source.unsplash.com/201x201/?random"
+            />
+            <img
+              alt=""
+              className="w-full h-full rounded shadow-sm min-h-48 "
+              src="https://source.unsplash.com/202x202/?random"
+            />
+            <img
+              alt=""
+              className="w-full h-full rounded shadow-sm min-h-48 "
+              src="https://source.unsplash.com/203x203/?random"
+            />
+            <img
+              alt=""
+              className="w-full h-full rounded shadow-sm min-h-48 "
+              src="https://source.unsplash.com/204x204/?random"
+            />
+            <img
+              alt=""
+              className="w-full h-full rounded shadow-sm min-h-48 "
+              src="https://source.unsplash.com/205x205/?random"
+            />
+            <img
+              alt=""
+              className="w-full h-full rounded shadow-sm min-h-48 "
+              src="https://source.unsplash.com/206x206/?random"
+            />
+            <img
+              alt=""
+              className="w-full h-full rounded shadow-sm min-h-48 "
+              src="https://source.unsplash.com/207x207/?random"
+            />
+            <img
+              src="https://source.unsplash.com/302x302/?random"
+              alt=""
+              className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-1 md:row-start-3 "
+            />
+          </div>
+        </section>
       </main>
     </DefaultLayout>
   );
