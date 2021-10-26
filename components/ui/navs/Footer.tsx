d603ad95a348ca437ee54369a65465b5df93dff3
ansango <anibalsantosgo@gmail.com>
2021-10-26 21:46:04 +0200

    colaboradores

--- components/ui/navs/Footer.tsx
+++ components/ui/navs/Footer.tsx
@@ -1,99 +1,102 @@
 import Link from "next/link";
 import { useI18n } from "next-localization";
-import { FBIcon, IGIcon, TWIcon, YTIcon } from "../icons";
+import { FBIcon, IGIcon } from "../icons";
 
 const Footer = () => {
   const { t } = useI18n();
   const currentYear = new Date().getFullYear();
   return (
-    <footer>
-      <div className="pt-10 pb-10">
-        <ul className="flex justify-center pb-3">
-          <li className="w-8 h-8  text-xl">
-            <FBIcon />
-          </li>
-          <li className="w-8 h-8  text-xl">
-            <TWIcon />
-          </li>
-          <li className="w-8 h-8  text-xl">
-            <IGIcon />
-          </li>
-          <li className="w-8 h-8  text-xl">
-            <YTIcon />
-          </li>
-        </ul>
-        <div className="flex justify-center text-center">
-          <ul className="grid grid-cols-2 md:grid-cols-4">
-            <li>Nosotros</li>
-            <li>Dónde estamos</li>
-            <li>Condiciones Generales</li>
-            <li>Ley de protección de datos</li>
-          </ul>
-        </div>
-      </div>
-      <div className="bg-primary text-white" aria-labelledby="footer-heading">
-        <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:px-16">
-          <div className="xl:grid xl:grid-cols-3 xl:gap-8">
-            <div className="text-white xl:col-span-1">
-              <Link href="/" passHref>
-                <span className="text-lg font-bold tracking-tighter transition duration-500 ease-in-out transform tracking-relaxed lg:pr-8">
-                  Árbore
-                </span>
-              </Link>
-              <p className="w-1/2 mt-2 text-sm">Consumo Consciente</p>
+    <footer className="bg-primary text-white" aria-labelledby="footer-heading">
+      <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:px-16">
+        <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+          <div className="text-white xl:col-span-1">
+            <Link href="/" passHref>
+              <span className="text-lg font-bold tracking-tighter transition duration-500 ease-in-out transform tracking-relaxed lg:pr-8">
+                Árbore
+              </span>
+            </Link>
+            <p className="w-1/2 mt-2 text-sm">Consumo Consciente</p>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-12 xl:mt-0 xl:col-span-2">
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 className="text-sm font-bold tracking-wider uppercase underline">
+                  {t("common.footer.menu.title")}
+                </h3>
+                <ul role="list" className="mt-4 space-y-2">
+                  {Object.entries(t("common.footer.menu.links")).map(([key, value]) => {
+                    return (
+                      <li className="hover:underline" key={key}>
+                        <Link href={`/${key}`}>
+                          <a>{value}</a>
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+              <div className="mt-12 md:mt-0">
+                <h3 className="text-sm font-bold tracking-wider uppercase underline">
+                  {t("common.footer.usefulLinks.title")}
+                </h3>
+                <ul role="list" className="mt-4 space-y-2">
+                  {Object.entries(t("common.footer.usefulLinks.links")).map(([key, value]) => {
+                    return (
+                      <li className="hover:underline" key={key}>
+                        <Link href={`/${key}`}>
+                          <a>{value}</a>
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </div>
-            <div className="grid grid-cols-2 gap-8 mt-12 xl:mt-0 xl:col-span-2">
-              <div className="md:grid md:grid-cols-2 md:gap-8">
-                <div>
-                  <h3 className="text-sm font-bold tracking-wider uppercase underline">
-                    {t("common.footer.menu.title")}
-                  </h3>
-                  <ul role="list" className="mt-4 space-y-2">
-                    {Object.entries(t("common.footer.menu.links")).map(([key, value]) => {
-                      return (
-                        <li className="hover:underline" key={key}>
-                          <Link href={`/${key}`}>
-                            <a>{value}</a>
-                          </Link>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </div>
-                <div className="mt-12 md:mt-0">
-                  <h3 className="text-sm font-bold tracking-wider uppercase underline">
-                    {t("common.footer.usefulLinks.title")}
-                  </h3>
-                  <ul role="list" className="mt-4 space-y-2">
-                    {Object.entries(t("common.footer.usefulLinks.links")).map(([key, value]) => {
-                      return (
-                        <li className="hover:underline" key={key}>
-                          <Link href={`/${key}`}>
-                            <a>{value}</a>
-                          </Link>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </div>
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 className="text-sm font-bold tracking-wider uppercase underline">Social</h3>
+                <ul role="list" className="mt-4 space-y-2">
+                  <li className="hover:underline">
+                    <a
+                      href="https://www.facebook.com/arborecooperativa"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="flex items-center"
+                    >
+                      Facebook
+                      <FBIcon className="ml-2" />
+                    </a>
+                  </li>
+                  <li className="hover:underline">
+                    <a
+                      href="https://www.instagram.com/arborecooperativa/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="flex items-center"
+                    >
+                      Instagram
+                      <IGIcon className="ml-2" />
+                    </a>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
         </div>
-        <div className="px-4 py-6 mx-auto bg-gray-200 max-w-7xl sm:px-6 lg:px-16">
-          <div className="flex flex-wrap items-baseline">
-            <span className="mt-2 text-sm font-light text-gray-500">
-              Copyright © {currentYear}
-              <a
-                href="http://arbore.org"
-                className="mx-2 text-wickedblue hover:text-gray-300"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                arbore
-              </a>
-            </span>
-          </div>
+      </div>
+      <div className="px-4 py-6 mx-auto bg-gray-200 max-w-7xl sm:px-6 lg:px-16">
+        <div className="flex flex-wrap items-baseline">
+          <span className="mt-2 text-sm font-light text-gray-500">
+            Copyright © {currentYear}
+            <a
+              href="http://arbore.org"
+              className="mx-2 text-wickedblue hover:text-gray-300"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              arbore
+            </a>
+          </span>
         </div>
       </div>
     </footer>
--- pages/about.tsx
+++ pages/about.tsx
@@ -249,42 +249,39 @@ const About: NextPage = () => {
           <div className="container px-5 py-24 mx-auto lg:px-16">
             <div className="flex flex-col w-full mb-8 text-center">
               <span className="mb-4 text-sm font-medium tracking-wide text-gray-500 uppercase">
-                {" "}
-                The best front end teams use Wickedtemplates to state their presence.{" "}
-                <a
-                  href="https://www.wickedtemplates.com/expo.html"
-                  className="font-semibold text-blue-400 lg:mb-0 hover:text-blue-500"
-                >
-                  Our customers »
-                </a>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
               </span>
             </div>
             <div className="mx-auto text-center">
-              <div className="grid grid-cols-4 gap-4 mx-auto">
-                <div>
-                  <Image src={colab1} />
-                </div>
-                <div>
-                  <Image src={colab2} />
-                </div>
-                <div>
-                  <Image src={colab3} />
-                </div>
-                <div>
-                  <Image src={colab4} />
-                </div>
-                <div>
-                  <Image src={colab5} />
-                </div>
-                <div>
-                  <Image src={colab6} />
-                </div>
-                <div>
-                  <Image src={colab7} />
-                </div>
-                <div>
-                  <Image src={colab8} />
-                </div>
+              <div className="grid grid-cols-2 gap-4 mx-auto md:grid-cols-4">
+                <a href="https://agavelaspg.org/" target="_blank" rel="noreferrer">
+                  <Image src={colab1} alt="colab1" />
+                </a>
+                <a href="https://www.coop57.coop/es/galiza" target="_blank" rel="noreferrer">
+                  <Image src={colab2} alt="colab2" />
+                </a>
+                <a href="https://espazo.coop/" target="_blank" rel="noreferrer">
+                  <Image src={colab3} alt="colab3" />
+                </a>
+                <a href="https://www.fiarebancaetica.coop/" target="_blank" rel="noreferrer">
+                  <Image src={colab4} alt="colab4" />
+                </a>
+                <a href="https://www.mercadosocial.net/" target="_blank" rel="noreferrer">
+                  <Image src={colab5} alt="colab5" />
+                </a>
+                <a
+                  href="https://www.nosaenerxia.gal/index.php/gl/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Image src={colab6} alt="colab6" />
+                </a>
+                <a href="https://www.reasred.org/reas-galicia" target="_blank" rel="noreferrer">
+                  <Image src={colab7} alt="colab7" />
+                </a>
+                <a href="https://somosconexion.coop/" target="_blank" rel="noreferrer">
+                  <Image src={colab8} alt="colab8" />
+                </a>
               </div>
             </div>
           </div>
